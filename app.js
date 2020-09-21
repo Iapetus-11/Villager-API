642ca9cd225c77431d21d2c3f7c83cf012cf8e81
Iapetus-11 <olimdahacker@gmail.com>
2020-09-21 09:25:37 -0400

    add ip filter

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const Express = require('express');
 const Helmet = require('helmet');
+const Fs = require('fs');
 
 const app = Express();
-
 app.use(Helmet());
+
+p = JSON.parse(Fs.readFileSync('private.json'))
+
 app.use(function(req, res, next){
   let ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
 
-  if ()
+  if (p.allowed.includes(ip)) {
+    next();
+  } else {
+    res.status(401).end();
+  }
 });
 
 app.listen(2304, () => {
