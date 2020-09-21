6aceac7f6e93f88839f246e24d55c21cb69078c5
Iapetus-11 <olimdahacker@gmail.com>
2020-09-21 09:17:37 -0400

    start working on limiting usage to only certain ips

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const Helmet = require('helmet');
 const app = Express();
 
 app.use(Helmet());
+app.use(function(req, res, next){
+  let ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+
+  if ()
+});
 
 app.listen(2304, () => {
   console.log('Server running.');
