0377b727ac89784dd1005370e06669850a3e3ba5
Iapetus-11 <olimdahacker@gmail.com>
2020-09-29 11:05:21 -0400

    rewrite to fix caching issues

--- routes/reddit.js
+++ routes/reddit.js
@@ -4,33 +4,35 @@ const Axios = require('axios');
 
 const router = Express.Router();
 
-var imagesCache = [];
+var imagesCache = []; // should be dictionary of lists {'subreddits': []}
 var lastUpdate;
 
 router.get('/gimme/:subreddits', (req, res) => {
-  let url = 'https://reddit.com/r/' + req.params.subreddits;
+  let subreddits = req.params.subreddits;
 
-  Axios.get(url)
-  .then(redditRes => {
-    if (redditRes.status != 200) {
-      res.status(500).json({success: false, error: 'Response status code (from Reddit) was not 200 OK.'});
-      return;
-    }
+  if (((new Date()) - lastUpdate) / 1000 >= 30 || imagesCache[subreddits].length < 1) { // update cache if last update 30 seconds or more ago or cache is empty
+    Axios.get('https://reddit.com/r/' + subreddits)
+    .then(redditRes => {
+      if (redditRes.status != 200) {
+        res.status(500).json({success: false, error: 'Response status code (from Reddit) was not 200 OK.'});
+        return;
+      }
 
-    if (((new Date()) - lastUpdate) / 1000 >= 30 || imagesCache.length < 1) { // update cache if last update 30 seconds or more ago or cache is empty
       let html = Cheerio.load(redditRes.data);
       let images = html('._2_tDEnGMLxpM6uOa2kaDB3.ImageBox-image.media-element._1XWObl-3b9tPy64oaG6fax');
 
-      imagesCache = [];
+      imagesCache[subreddits] = [];
 
       for (i = 0; i < images.length; i++) {
-        imagesCache.push(images[i].attribs.src);
+        imagesCache[subreddits].push(images[i].attribs.src);
       }
-    }
 
+      res.status(200).json({success: true, imageUrl: imagesCache[Math.round(Math.random() * imagesCache.length)]});
+    })
+    .catch(e => console.log(e));
+  } else {
     res.status(200).json({success: true, imageUrl: imagesCache[Math.round(Math.random() * imagesCache.length)]});
-  })
-  .catch(e => console.log(e));
+  }
 });
 
 module.exports = router;
