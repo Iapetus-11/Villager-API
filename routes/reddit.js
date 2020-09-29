76aa45d77a74d81b751e7a7b72b0ab69e7bf3b6d
Iapetus-11 <olimdahacker@gmail.com>
2020-09-29 10:25:54 -0400

    fix typo

--- routes/reddit.js
+++ routes/reddit.js
@@ -16,9 +16,10 @@ router.get('/gimme/:subreddits', (req, res) => {
 
     let html = Cheerio.load(redditRes.data);
     let images = html('._2_tDEnGMLxpM6uOa2kaDB3.ImageBox-image.media-element._1XWObl-3b9tPy64oaG6fax');
-    let post = images.initialize[Math.round(Math.random() * images.length)];
 
-    res.status(200).json({success: true, imageUrl: image.attribs.src});
+    let post = images[Math.round(Math.random() * images.length)];
+
+    res.status(200).json({success: true, imageUrl: post.attribs.src});
   })
   .catch(e => console.log(e));
 });
