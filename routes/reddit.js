9da897802fe0e7674bfc677fdb67319017323425
Iapetus-11 <olimdahacker@gmail.com>
2020-09-29 11:56:33 -0400

    rewrite reddit endpoints to use the reddit api rather than scraping data

--- routes/reddit.js
+++ routes/reddit.js
@@ -1,5 +1,4 @@
 const Express = require('express');
-const Cheerio = require('cheerio');
 const Axios = require('axios');
 
 const router = Express.Router();
@@ -11,29 +10,42 @@ router.get('/gimme/:subreddits', (req, res) => {
   let subreddits = req.params.subreddits;
 
   if (((new Date()) - lastUpdate) / 1000 >= 30 || imagesCache[subreddits] == undefined || imagesCache[subreddits].length < 1) { // update cache if last update 30 seconds or more ago or cache is empty
-    Axios.get('https://reddit.com/r/' + subreddits)
+    Axios.get(`https://reddit.com/r/${subreddits}/hot/.json?count=100`)
     .then(redditRes => {
       if (redditRes.status != 200) {
         res.status(500).json({success: false, error: 'Response status code (from Reddit) was not 200 OK.'});
         return;
       }
 
-      let html = Cheerio.load(redditRes.data);
-      let images = html('._2_tDEnGMLxpM6uOa2kaDB3.ImageBox-image.media-element._1XWObl-3b9tPy64oaG6fax');
-
       imagesCache[subreddits] = [];
+      let posts = redditRes.data.data.children;
 
-      for (i = 0; i < images.length; i++) {
-        imagesCache[subreddits].push(images[i].attribs.src);
+      for (i = 0; i < posts.length; i++) {
+        let postData = posts[i].data;
+
+        if (!(postData.removal_reason || postData.is_video || postData.pinned || postData.stickied || postData.selftext)) {
+          imagesCache[subreddits].push({
+            id: postData.id,
+            subreddit: postData.subreddit,
+            author: postData.author,
+            title: postData.title,
+            permalink: postData.permalink,
+            url: postData.url,
+            upvotes: postData.ups,
+            downs: postData.downs,
+            nsfw: postData.over_18,
+            spoiler: postData.spoiler
+          });
+        }
       }
 
-      let image = imagesCache[subreddits][Math.round(Math.random() * imagesCache.length)];
-      res.status(200).json({success: true, imageUrl: image});
+      let post = imagesCache[subreddits][Math.floor(Math.random() * imagesCache.length)];
+      res.status(200).json(Object.assign({}, {success: true}, post));
     })
     .catch(e => console.log(e));
   } else {
-    let image = imagesCache[subreddits][Math.round(Math.random() * imagesCache.length)];
-    res.status(200).json({success: true, imageUrl: image});
+    let post = imagesCache[subreddits][Math.floor(Math.random() * imagesCache.length)];
+    res.status(200).json(Object.assign({}, {success: true}, post));
   }
 });
 
