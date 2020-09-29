2e00de63af6dcd5a936b69c793067041b5a7999e
Iapetus-11 <olimdahacker@gmail.com>
2020-09-29 12:13:44 -0400

    increase speed of serving requests

--- routes/reddit.js
+++ routes/reddit.js
@@ -6,41 +6,57 @@ const router = Express.Router();
 var imagesCache = []; // should be dictionary of lists {'subreddits': []}
 var lastUpdate;
 
+async def updateCache(subreddits, limit) {
+  Axios.get(`https://reddit.com/r/${subreddits}/hot/.json?limit=${limit}`)
+  .then(redditRes => {
+    if (redditRes.status != 200) {
+      return false;
+    }
+
+    imagesCache[subreddits] = [];
+    let posts = redditRes.data.data.children;
+
+    for (i = 0; i < posts.length; i++) {
+      let postData = posts[i].data;
+
+      if (!(postData.removal_reason || postData.is_video || postData.pinned || postData.stickied || postData.selftext)) {
+        imagesCache[subreddits].push({
+          id: postData.id,
+          subreddit: postData.subreddit,
+          author: postData.author,
+          title: postData.title,
+          permalink: 'https://reddit.com' + postData.permalink,
+          url: postData.url,
+          upvotes: postData.ups,
+          downs: postData.downs,
+          nsfw: postData.over_18,
+          spoiler: postData.spoiler
+        });
+      }
+    }
+
+    return true;
+  })
+  .catch(e => {
+    console.log(e);
+    return false;
+  });
+}
+
 router.get('/gimme/:subreddits', (req, res) => {
   let subreddits = req.params.subreddits;
 
   if (((new Date()) - lastUpdate) / 1000 >= 60 || imagesCache[subreddits] == undefined || imagesCache[subreddits].length < 1) { // update cache if last update 30 seconds or more ago or cache is empty
-    Axios.get(`https://reddit.com/r/${subreddits}/hot/.json?limit=75`)
-    .then(redditRes => {
-      if (redditRes.status != 200) {
-        res.status(500).json({success: false, error: 'Response status code (from Reddit) was not 200 OK.'});
-        return;
-      }
-
-      imagesCache[subreddits] = [];
-      let posts = redditRes.data.data.children;
-
-      for (i = 0; i < posts.length; i++) {
-        let postData = posts[i].data;
-
-        if (!(postData.removal_reason || postData.is_video || postData.pinned || postData.stickied || postData.selftext)) {
-          imagesCache[subreddits].push({
-            id: postData.id,
-            subreddit: postData.subreddit,
-            author: postData.author,
-            title: postData.title,
-            permalink: 'https://reddit.com' + postData.permalink,
-            url: postData.url,
-            upvotes: postData.ups,
-            downs: postData.downs,
-            nsfw: postData.over_18,
-            spoiler: postData.spoiler
-          });
-        }
-      }
-
+    updateCache(subreddits, 10)
+    .then(success => {
       let post = imagesCache[subreddits][Math.floor(Math.random() * imagesCache[subreddits].length)];
       res.status(200).json(Object.assign({}, {success: true}, post));
+
+      if (success) {
+        updateCache(subreddits, 75)
+        .then(success => {})
+        .catch(e => console.log(e));
+      }
     })
     .catch(e => console.log(e));
   } else {
