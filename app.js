09dd6cf005010f51541c9966b7fa602dd950c6ef
Iapetus-11 <olimdahacker@gmail.com>
2020-10-01 20:47:31 -0400

    remove old auth system

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const p = JSON.parse(Fs.readFileSync('private.json'));
 const app = Express();
 
 app.use(Helmet());
-app.use(function(req, res, next){
-  if (req.get('Authorization') == p.auth || (req.query.k == p.query_k && ['/mc/mcpingimg', '/mc/serverfavi'].includes(req.path))) {
-    next(); // move on to next middleware
-  } else {
-    res.status(401).end(); // 401 unauthed
-  }
-});
+
+// old auth system
+// app.use(function(req, res, next){
+//   if (req.get('Authorization') == p.auth || (req.query.k == p.query_k && ['/mc/mcpingimg', '/mc/serverfavi'].includes(req.path))) {
+//     next(); // move on to next middleware
+//   } else {
+//     res.status(401).end(); // 401 unauthed
+//   }
+// });
 
 app.use('/mc', require('./routes/mc'));
 app.use('/reddit', require('./routes/reddit'));
