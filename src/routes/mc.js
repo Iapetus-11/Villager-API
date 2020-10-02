4cb9e9b3535a7773393ad257480500a737c51750
Iapetus-11 <olimdahacker@gmail.com>
2020-10-02 00:19:31 -0400

    add utility imports

--- src/routes/mc.js
+++ src/routes/mc.js
@@ -2,3 +2,9 @@ import Express from 'express';
 import Canvas from 'canvas';
 import Axios from 'axios';
 import Fs from 'fs';
+
+// import utility functions
+import * as CUtil from '../util/canvas.js';
+import * as MCUtil from '../util/mc.js';
+
+Canvas.registerFont('../assets/Minecraftia.ttf', {family: 'Minecraft', style: 'normal'});
