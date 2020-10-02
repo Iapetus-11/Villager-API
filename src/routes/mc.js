63234e97a47ec3375abb450706ac7581319f66ae
Iapetus-11 <olimdahacker@gmail.com>
2020-10-02 00:36:05 -0400

    add /mcstatus endpoint

--- src/routes/mc.js
+++ src/routes/mc.js
@@ -4,7 +4,32 @@ import Axios from 'axios';
 import Fs from 'fs';
 
 // import utility functions
-import * as CUtil from '../util/canvas.js';
+import * as CnvsUtil from '../util/canvas.js';
 import * as MCUtil from '../util/mc.js';
 
 Canvas.registerFont('../assets/Minecraftia.ttf', {family: 'Minecraft', style: 'normal'});
+
+const router = Express.Router();
+
+router.get('/mcstatus/:mcserver', (req, res) => {
+  let mcserver = req.params.mcserver;
+
+  if (mcserver.length > 200) {
+    res.status(400).json({success: false, message: 'The mcserver parameter must not be longer than 200 characters'});
+    return;
+  }
+
+  if (mcserver.length < 3) {
+    res.status(400).json({success: false, message: 'The mcserver parameter must be longer than 2 characters'});
+    return;
+  }
+
+  MCUtil.status(mcserver)
+  .then(status => {
+    res.status(200).json(Object.assign({success: true}, status));
+  })
+  .catch(e => {
+    console.log(e);
+    res.status(500).json({success: false, message: 'Oops... Something went wrong on our end'});
+  });
+});
