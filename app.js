32ddc57f38c11005893a84d574db2aa77126e4ea
Iapetus-11 <olimdahacker@gmail.com>
2020-10-01 21:13:41 -0400

    finish ratelimiting and add handlers + prettify code

--- app.js
+++ app.js
@@ -5,16 +5,40 @@ const Fs = require('fs');
 
 require('dotenv').config(); // basically initialize dotenv
 
-const app = Express();
-
-app.use(Helmet());
+function limitHandler(req, res) {
+  res.status(429).json({
+    success: false,
+    message: 'Too many requests! You have hit the rate limit.',
+    limit: req.rateLimit.limit,
+    current: req.rateLimit.current,
+    remaining: req.rateLimit.remaining
+  });
+  res.end();
+}
 
 function skipHandler(req, res) {
   return (process.env.BYPASS_AUTH == req.get('Authorization'));
 }
 
-app.use('/reddit', RateLimit({windowMs: 20*1000, max: 2, skip: skipHandler}), require('./routes/reddit'));
-app.use('/mc', RateLimit({windowMs: 30*1000, max: 2, skip: skipHandler}), require('./routes/mc'));
+const app = Express();
+
+const redditRateLimiter = RateLimit({
+  windowMs: 20*1000,
+  max: 3,
+  skip: skipHandler,
+  handler: limitHandler
+});
+
+const mcRateLimiter = RateLimit({
+  windowMs: 30*1000,
+  max: 3,
+  skip: skipHandler,
+  handler: limitHandler
+});
+
+app.use(Helmet());
+app.use('/reddit', redditRateLimiter, require('./routes/reddit'));
+app.use('/mc', mcRateLimiter, require('./routes/mc'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}.`);
