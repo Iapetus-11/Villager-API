34bcced130615a774cc0653a7081ed8f992c0af7
Iapetus-11 <olimdahacker@gmail.com>
2020-10-02 00:45:02 -0400

    add /mcstatusimg/ route

--- src/routes/mc.js
+++ src/routes/mc.js
@@ -19,14 +19,44 @@ router.get('/mcstatus/:mcserver', (req, res) => {
     return;
   }
 
-  if (mcserver.length < 3) {
-    res.status(400).json({success: false, message: 'The mcserver parameter must be longer than 2 characters'});
+  if (mcserver.length < 5) {
+    res.status(400).json({success: false, message: 'The mcserver parameter must be longer than 4 characters'});
     return;
   }
 
   MCUtil.status(mcserver)
   .then(status => {
     res.status(200).json(Object.assign({success: true}, status));
+  })
+  .catch(e => {
+    console.log(e);
+    res.status(500).json({success: false, message: 'Oops... Something went wrong on our end'});
+  });
+});
+
+router.get('/mcstatusimg/:mcserver', (req, res) => {
+  let mcserver = req.params.mcserver;
+
+  if (mcserver.length > 200) {
+    res.status(400).json({success: false, message: 'The mcserver parameter must not be longer than 200 characters'});
+    return;
+  }
+
+  if (mcserver.length < 5) {
+    res.status(400).json({success: false, message: 'The mcserver parameter must be longer than 4 characters'});
+    return;
+  }
+
+  MCUtil.status(mcserver)
+  .then(status => {
+    MCUtil.genStatusCard(status)
+    .then(image => {
+      CnvsUtil.sendImage(image, res, 'status.png');
+    })
+    .catch(e => {
+      console.log(e);
+      res.status(500).json({success: false, message: 'Oops... Something went wrong on our end'});
+    });
   })
   .catch(e => {
     console.log(e);
