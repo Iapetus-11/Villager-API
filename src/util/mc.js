1a952b5de27313d8de2868e97d6a45637b71a317
Iapetus-11 <olimdahacker@gmail.com>
2020-10-02 00:29:12 -0400

    update status function in mc util functions

--- src/util/mc.js
+++ src/util/mc.js
@@ -1,10 +1,10 @@
 
-export function status(host, port, stop) {
+export function status(mcserver, stop) {
   return new Promise((resolve, reject) => {
-    Axios.get('http://localhost:2304/mcstatus', {data: {host: host, port: port}})
+    Axios.get('http://localhost:2304/mcstatus', {data: {mcserver: mcserver}})
     .then(resp => {
       if (!resp.data.motd && !stop) {
-        resolve(status(host, port, true));
+        resolve(status(mcserver, true));
       } else {
         resolve(resp.data);
       }
