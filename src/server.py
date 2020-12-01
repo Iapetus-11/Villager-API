0cbdc7b32052bbf54fff596f6d536d4f147b01a5
Iapetus-11 <olimdahacker@gmail.com>
2020-12-01 11:11:31 -0500

    increase wait time

--- src/server.py
+++ src/server.py
@@ -160,7 +160,7 @@ async def unified_mcping(server_str, _port=None, _ver=None, *, do_resolve=False)
         for c in ip:
             if c in abcd:
                 try:
-                    d_ans = await asyncio.wait_for(dns.asyncresolver.resolve(f'_minecraft._tcp.{ip}', 'SRV', search=True, lifetime=10), .5)
+                    d_ans = await asyncio.wait_for(dns.asyncresolver.resolve(f'_minecraft._tcp.{ip}', 'SRV', search=True, lifetime=10), 1)
                     return await unified_mcping(f'{d_ans[0].target.to_text().strip(".")}:{d_ans[0].port}')
                 except Exception as e:
                     break
