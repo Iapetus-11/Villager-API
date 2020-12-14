1135d87f8cff1470f721d14b0716302e7919e1bb
Iapetus-11 <olimdahacker@gmail.com>
2020-12-14 11:18:17 -0500

    attempt to fix rotation + position of text

--- src/util/mc.js
+++ src/util/mc.js
@@ -281,8 +281,8 @@ export function genSplashScreen(text) {
 
     CnvsUtil.drawImageAsync(ctx, './src/assets/mcsplash.png', 0, 0, 512, 271)
     .then(() => {
-      ctx.rotate(-0.35);
-      CnvsUtil.drawTextAsync(ctx, text, 370, 69+200, 'Minecraft', '#FFFF55', 12, 400, 'center')
+      ctx.rotate(-0.45);
+      CnvsUtil.drawTextAsync(ctx, text, 370-200, 69+200, 'Minecraft', '#FFFF55', 12, 400, 'center')
       .then(() => {
         resolve(image);
       })
