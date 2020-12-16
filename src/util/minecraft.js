6f75865cc697937f7ca8f125893dff2a362d6eca
Iapetus-11 <olimdahacker@gmail.com>
2020-12-15 19:24:33 -0500

    add util/minecraft.js and mcstatus + mcstatus cache

---
+++ src/util/minecraft.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+var mcstatusCache = {}; // { 'server': { data } }
+
+function clearCacheLoop() { // Clear old / outdated statuses from the cache
+  Object.keys(mcstatusCache).forEach(key => {
+    if ((new Date() - mcstatusCache[key].cacheTime)/1000 > 30) {
+      delete mcstatusCache[key];
+    }
+  });
+}
+
+setInterval(clearCacheLoop, 1000); // Make clearCacheLoop run every second
+
+export function mcstatus(mcserver) {
+  return new Promise((resolve, reject) => {
+    let cached = mcstatusCache[mcserver];
+
+    if (cached) {
+      return cached;
+    } else {
+      axios.get('http://localhost:2304/mcstatus', { data: { mcserver: mcserver } })
+      .then(res => {
+        let status = Object.assign(res.data, { cached: false, cacheTime: null });
+
+        resolve(status);
+
+        // Insert into cache
+        mcstatusCache[mcserver] = Object.assign(Object.assign({}, res.data), { cached: true, cacheTime: (new Date()) });
+      })
+      .catch(e => reject(e));
+    }
+  });
+}
