cedb8f83d5a88885e9b4aebed87b728b09477659
Iapetus-11 <olimdahacker@gmail.com>
2020-12-15 21:59:57 -0500

    add mcstatus endpoint

---
+++ src/routes/mc/mcstatus.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import {mcstatus} from '../../util/minecraft.js';
+
+const router = express.Router();
+
+router.get('/mcstatus/:mcserver', async (req, res) => {
+  let mcserver = req.params.mcserver;
+
+  if (4 > mcserver.length > 150) {
+    res.status(400).json({success: false, message: 'Bad Request - URL parameter mcserver is invalid'});
+    return;
+  }
+
+  let status = await mcstatus(mcserver);
+  res.status(200).json({success: true, ...status});
+});
+
+export default router;
