304d3eaaf2e4692a3deae2ab6f724f05d45170e4
Milo Weinberg <iapetus011@gmail.com>
2021-01-16 10:44:33 -0500

    use TIMEOUT variable in call to as_completed()

--- src/server.py
+++ src/server.py
@@ -117,7 +117,7 @@ async def mcstatus(host, port, do_resolve=False):
     statuses = (ping_status(host, port), raknet_status(host, port),)
 
     try:
-        for status in asyncio.as_completed(statuses, timeout=2):
+        for status in asyncio.as_completed(statuses, timeout=TIMEOUT):
             status = await status
 
             if status['online']:
